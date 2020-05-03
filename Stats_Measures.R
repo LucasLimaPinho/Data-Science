9fca17ae25b8c61c186c1ab597518eeffffa2d58
Lucas Lima <64433440+LucasLimaPinho@users.noreply.github.com>
2020-05-03 20:31:22 -0300

    Add files via upload

---
+++ Stats_Measures.R
@@ -0,0 +1,8 @@
+soccer_players = c(40000, 18000, 12000, 250000, 30000, 
+                   140000, 300000, 40000,800000)
+mean(soccer_players)
+median(soccer_players)
+quantile = quantile(soccer_players)
+standard_deviation = sd (soccer_players)
+summary(soccer_players)
+standard_deviation
