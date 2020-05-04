09821140bef20f8236ff247d181bb575df8d8ab5
Lucas Lima <64433440+LucasLimaPinho@users.noreply.github.com>
2020-05-04 05:11:02 -0300

    Add files via upload

---
+++ Poison_Distribution.R
@@ -0,0 +1,13 @@
+# Poison Distribution
+# @author: LucasLimaPinho
+
+# mean = 2 accidents
+# x = 3
+dpois(3,lambda = 2)
+
+# x <=3
+ppois(3,lambda = 2)
+
+# x > 3
+
+ppois(3,lambda=2,lower.tail = F)
