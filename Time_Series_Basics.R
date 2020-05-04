c2088159def1b0dd7e44fa8dd79a202397eb8781
Lucas Lima <64433440+LucasLimaPinho@users.noreply.github.com>
2020-05-04 14:39:34 -0300

    Add files via upload

---
+++ Time_Series_Basics.R
@@ -0,0 +1,10 @@
+#Time Series
+
+AirPassengers
+start(AirPassengers)
+end(AirPassengers)
+plot(AirPassengers)
+plot(aggregate(AirPassengers))
+monthplot(AirPassengers)
+subst = window(AirPassengers, start = c(1960,1), end=c(1960,12))
+plot(subst)
